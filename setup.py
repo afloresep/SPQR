fad1654c5e9fd8342b7f25017ccf0ae3cc8ad636
afloresep <afloresep01@gmail.com>
2025-03-17 15:53:51 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spiq_project",
+    version="0.1.0",
+    description="Streaming Product Quantization pipeline for large-scale clustering of molecular data.",
+    author="Alejandro Flores",
+    author_email="afloresep01@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "Rdkit", 
+        "pandas"
+    ],
+    entry_points={
+        "console_scripts": [
+            "spq=spq.main:main"
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
